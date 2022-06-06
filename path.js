d40acc1bdb9d1228ecc24b8922d7f26970725494
Alexandr Borodinov <yaborodinov@gmail.com>
2022-06-06 21:26:47 +0300

    added path methods

--- path.js
+++ path.js
@@ -1,7 +1,11 @@
-const path = require('path')
+const chalk = require('chalk')
+
+const path = require('path')    // типа некая библиотека path
 
 console.log('file name is', path.basename(__filename));  // метод для названия файла
 
 console.log('directory name is', path.dirname(__filename));   // метод чтоб узнать директорию
 
-console.log();
+console.log(chalk.green('file extention', path.extname(__filename)));   // метод чтоб узнать расширение файла
+
+console.log('returns obj', path.parse(__filename));   // метод, возвращающий объект с ключами и значениями данного
