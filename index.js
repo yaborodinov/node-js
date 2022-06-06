2dde4ab79e2043508f20f8fe940e1203d6426281
Alexandr Borodinov <yaborodinov@gmail.com>
2022-06-06 20:58:00 +0300

    used metods to path

---
+++ index.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk')   //  с помощью requere мы подключаем пакет, в данном случае chalk
+const text = require('./data')   // называть переменную можно как угодно, не обязательно text
+
+
+console.log(chalk.green(text));
---
+++ path.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+console.log('file name is', path.basename(__filename));  // метод для названия файла
+
+console.log('directory name is', path.dirname(__filename));   // метод чтоб узнать директорию
+
+console.log();
